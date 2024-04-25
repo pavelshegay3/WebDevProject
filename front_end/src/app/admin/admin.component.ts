6f604b13fc3677f7f951e73d438032d39f8d8616
pavelshegay3 <pavel1155.shegay@gmail.com>
2024-04-25 14:52:31 +0500

    Post category works

--- front_end/src/app/admin/admin.component.ts
+++ front_end/src/app/admin/admin.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Category } from '../services/category.interface';
+import { CategoryService } from '../services/category.service';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
 })
-export class AdminComponent {
+export class AdminComponent implements OnInit{
+  categories: Category[] = [];
+  newCategory: Category = { id: 0, name: '' };
 
+  constructor(private categoryService: CategoryService) { }
+
+  ngOnInit(): void {
+    this.getCategories();
+  }
+
+  getCategories(): void {
+    this.categoryService.getCategories()
+      .subscribe(categories => this.categories = categories);
+  }
+
+  createCategory(): void {
+    if (this.newCategory.name.trim()) {
+      this.categoryService.createCategory(this.newCategory)
+        .subscribe(category => {
+          this.categories.push(category);
+          this.newCategory.name = '';
+        });
+    }
+  }
 }
